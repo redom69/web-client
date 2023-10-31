06ed6847944b527c30fb12f8f3c2856466ed1973
Daniel Hernandez Puerto <sirdanielsan64@gmail.com>
2023-10-31 17:39:47 +0100

    FEAT: Project creation

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Button } from 'semantic-ui-react'
+
+function App() {
+  return (
+    <div >
+      <h1>Estamos en mi web</h1>
+      <Button primary>Primary</Button>
+      <Button secondary>Secondary</Button>
+    </div>
+  );
+}
+
+export default App;
