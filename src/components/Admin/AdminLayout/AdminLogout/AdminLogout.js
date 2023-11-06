1499303818058323916c7736ee7b3c44de5fe9f6
Daniel Hernandez Puerto <sirdanielsan64@gmail.com>
2023-11-06 14:19:46 +0100

    FEAT: Admin structure

---
+++ src/components/Admin/AdminLayout/AdminLogout/AdminLogout.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Button, Icon } from 'semantic-ui-react'
+import { useNavigate } from 'react-router-dom';
+
+import { useAuth } from '../../../../hooks';
+
+export function AdminLogout() {
+    const { logout } = useAuth();
+    const navigate = useNavigate();
+
+    const onLogout = () => {
+        logout();
+        navigate("/admin");
+    }
+
+    return (
+        <Button icon basic color='red' onClick={onLogout}>
+            <Icon name="power off" />
+            Cerrar sesion
+        </Button>
+    )
+}
--- src/layouts/AdminLayout/AdminLayout.js
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -1,12 +1,27 @@
 import React from 'react'
 
+import { AdminLogout } from '../../components/Admin/AdminLayout'
+import { AdminMenu } from '../../components/Admin/AdminLayout'
+import { Icon } from "../../assets"
+import "./AdminLayout.scss"
+
 export function AdminLayout(props) {
     const { children } = props
 
     return (
-        <div>
-            <h2>Se esta usando el admin layout</h2>
-            {children}
+        <div className='admin-layout'>
+            <div className='admin-layout__left'>
+                <Icon.LogoWhite className='logo' />
+                <AdminMenu />
+            </div>
+            <div className='admin-layout__right'>
+                <div className='admin-layout__right-header'>
+                    <AdminLogout />
+                </div>
+                <div className='admin-layout__right-content'>
+                    {children}
+                </div>
+            </div>
         </div>
     )
 }
